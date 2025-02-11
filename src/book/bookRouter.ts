87e3181253768ebd642e76ca1398f4ad7d91b22c
Piyush kumar <piyush2004.dev@gmail.com>
2025-02-11 13:06:11 +0530

    add multer middleware for file uploads and integrate with book creation endpoint

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { createBook } from "./bookController";
 const bookRouter = Router();
+import upload from "../middlewares/multer.middleware";
 
 // prefix - api/books
-bookRouter.post("/", createBook);
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "file", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  createBook
+);
 
 export default bookRouter;
